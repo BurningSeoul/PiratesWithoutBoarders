24312fdcc02db49e15292ef6a85c6ad794460103
Richard Als <richard.als@hotmail.com>
2018-03-02 11:45:20 -0500

    working on highscore

--- Assets/PirateShipScene/Scripts/GameManager.cs
+++ Assets/PirateShipScene/Scripts/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour {
     private float time = 4;
     public GameObject highScoreText;
     public GameObject yourScoreText;
+    private bool b_checkScore = false;
 
     private float ballsRemaining = 30;
     public Text shotsUI;
@@ -34,15 +35,21 @@ public class GameManager : MonoBehaviour {
     public Text EndScreenScore;
 	// Use this for initialization
 	void Start () {
+        //PlayerPrefs.DeleteAll();
         highScores = new float[10];
         names = new string[10];
+        b_checkScore = false;
+        time = 4;
 
         if (PlayerPrefs.HasKey("HighScore"))
         {
             for(int i = 0; i < 10; i++)
             {
-                highScores[i] = PlayerPrefs.GetInt("HighScore" + i);
+                highScores[i] = PlayerPrefs.GetFloat("HighScore" + i);
+                PlayerPrefs.SetFloat("HighScore" + i, highScores[i]);
                 highScoreNums[i].text = highScores[i].ToString();
+                PlayerPrefs.Save();
+                Debug.Log(highScores[i]);
             }
         }
 
@@ -50,17 +57,20 @@ public class GameManager : MonoBehaviour {
         {
             for(int i = 0; i < 10; i++)
             {
-                PlayerPrefs.SetInt("HighScore" + i, 0);
-                highScores[i] = PlayerPrefs.GetInt("HighScore" + i);
+                Debug.Log("testing3");
+                PlayerPrefs.SetFloat("HighScore" + i, 0);
+                PlayerPrefs.Save();
+                highScores[i] = PlayerPrefs.GetFloat("HighScore" + i);
                 highScoreNums[i].text = highScores[i].ToString();
+                PlayerPrefs.Save();
             }
         }
 
-        if (PlayerPrefs.HasKey("Names"))
+        if (PlayerPrefs.HasKey("Names1"))
         {
             for(int i = 0; i < 0; i++)
             {
-                names[i] = PlayerPrefs.GetString("Names" + i);
+                names[i] = PlayerPrefs.GetString("Names1" + i);
                 highScoreNames[i].text = names[i];
             }
         }
@@ -68,6 +78,9 @@ public class GameManager : MonoBehaviour {
         {
             for (int i = 0; i < 0; i++)
             {
+                PlayerPrefs.SetString("Names1" + i, "AAA");
+                names[i] = PlayerPrefs.GetString("Names1" + i);
+                PlayerPrefs.Save();
                 highScoreNames[i].text = "AAA";
             }
         }
@@ -107,6 +120,12 @@ public class GameManager : MonoBehaviour {
                 if(ballsRemaining <= 0)
                 {
                     currentState = GameState.HIGHSCORE;
+                    if (!b_checkScore)
+                    {
+                        CheckHighScores();
+                        b_checkScore = true;
+                    }
+                    
                 }
                 break;
             case GameState.GAMEOVER:
@@ -114,6 +133,7 @@ public class GameManager : MonoBehaviour {
                 {
                     go.SetActive(false);
                 }
+                highScorePanel.SetActive(false);
                 EndScreenPanel.SetActive(true);
                 EndScreenScore.text = currentScore.ToString();
                 break;
@@ -121,6 +141,7 @@ public class GameManager : MonoBehaviour {
                 highScorePanel.SetActive(true);
                 if (startTimer)
                 {
+                    
                     time -= Time.deltaTime;
 
                     if(time <= 0)
@@ -148,7 +169,7 @@ public class GameManager : MonoBehaviour {
         }
             
     }
-    public void AdjustScore(int score)
+    public void AdjustScore(float score)
     {
         if (currentState == GameState.GAMEPLAY)
         {
@@ -166,37 +187,57 @@ public class GameManager : MonoBehaviour {
 
     public void UpdateHighScore()
     {
-        for (int i = 0; i < 0; i++)
+        for (int i = 0; i < 10; i++)
         {
             if (currentScore > highScores[i])
             {
+                /*TempScore1 = highScores[i];
+                highScores[i] = TempScore;
+                TempScore = TempScore1;
+
+                TempName2 = names[i];
+                names[i] = TempName;
+                TempName = TempName2;*/
+
+
+
+
                 float temp = highScores[i];
                 highScores[i] = currentScore;
-                currentScore = temp;
+                highScoreNums[i].text = highScores[i].ToString();
+                //currentScore = highScores[i];
 
                 string tempN = names[i];
                 names[i] = tempName;
+                highScoreNames[i].text = names[i];
                 tempName = tempN;
+                //Debug.Log(highScores[i]);
+                //Debug.Log(names[i]);
+                //Debug.Log(tempN);
             }
         }
 
         for(int i = 0; i < 10; i++)
         {
-            PlayerPrefs.SetFloat("HighScore", highScores[i]);
-            PlayerPrefs.SetString("Names", names[i]);
+            
+            PlayerPrefs.SetFloat("HighScore" + 1, highScores[i]);
+            PlayerPrefs.Save();
+            PlayerPrefs.SetString("Names1" + 1, names[i]);
+            PlayerPrefs.Save();
         }
 
         startTimer = true;
-        PlayerPrefs.Save();
+        //PlayerPrefs.Save();
     }
 
     private void CheckHighScores()
     {
-        highScoreText.SetActive(false);
-        yourScoreText.SetActive(false);
-        for (int i = 0; i < 0; i++)
+        yourScoreText.GetComponent<Text>().text = currentScore.ToString();
+        highScoreText.SetActive(true);
+        yourScoreText.SetActive(true);
+        for (int i = 0; i < 10; i++)
         {
-            if (currentScore > highScores[i])
+            if (currentScore >= highScores[i])
             {
                 
                 inputF.SetActive(true);
@@ -205,7 +246,7 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    public void EnterName() {
+    public void EnterName() {   
         tempName = input.text;
         UpdateHighScore();
     }
